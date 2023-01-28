ed42fce802461357f1400469357c7a49c86e9123
NguyenDrasp <datngobhlc@gmail.com>
2023-01-28 08:25:30 +0700

    SparkStream

--- pipeline/API.py
+++ pipeline/API.py
@@ -7,6 +7,8 @@ from cassandra.auth import PlainTextAuthProvider
 from cassandra.query import dict_factory
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import spark
+from util.config import config
 # on the terminal type: curl http://127.0.0.1:5000/
 
 
@@ -98,6 +100,49 @@ def computeRSI (data, time_window):
     rs = abs(up_chg_avg/down_chg_avg)
     rsi = 100 - 100/(1+rs)
     return rsi
+
+def getPrices(code):
+
+    sql_query = "SELECT * FROM {}.{} WHERE SYMBOL = '{}' ORDER BY time DESC LIMIT 100".format("stocks", "historical", code)
+    df = pd.DataFrame()
+    for row in session.execute(sql_query):
+        df = df.append(pd.DataFrame(row, index=[0]))
+    print(df.head())
+    df['RSI'] = computeRSI(df['adjusted_close'], 14)
+    df = df.reset_index(drop=True).fillna(pd.np.nan)
+
+    return df.to_json()
+@app.route('/stream', methods = ['GET'])
+def SparkStreamd():
+
+    spark = SparkSession.builder.appName("StructuredNetworkWordCount").getOrCreate()
+
+    raw_df = spark.readStream.format("kafka")
+        .option("kafka.bootstrap.servers", config['kafka_broker'])
+        .option("subscribe",  config['topic_name2'])
+        .load().selectExpr("CAST(value AS STRING)")
+    
+    schema = StructType(
+    [
+            StructField("symbol", StringType()),
+            StructField("time", StringType()),
+            StructField("open", FloatType()),
+            StructField("high", FloatType()),
+            StructField("low", FloatType()),
+            StructField("close", FloatType()),
+            StructField("volume", StringType()),
+            StructField("last_trading_day", StringType()),
+            StructField("previous_close", StringType()),
+            StructField("change", StringType()),
+            StructField("change_percent", StringType()),
+            
+    ])
+    stock_df = raw_df.select(from_json(raw_df.value, schema).alias("data"))
+    stock_df.writeStream
+      .format("console")
+      .outputMode("append")
+      .start()
+      .awaitTermination()
 
 if __name__ == '__main__':
     app.run(debug = True)
