6104510ca88131ddd4466fba19e708977be44103
NguyenDrasp <datngobhlc@gmail.com>
2023-02-08 07:50:32 +0700

    chuyen tinh toan sang compute.py

--- pipeline/API.py
+++ pipeline/API.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import spark
 from util.config import config
+from compute import computeRSI, calEMA
 # on the terminal type: curl http://127.0.0.1:5000/
 
 
@@ -76,40 +77,6 @@ def getPrices(code):
     df = df.reset_index(drop=True).fillna(pd.np.nan)
 
     return df.to_json()
-
-def computeRSI (data, time_window = 14):
-    diff = data.diff(1).dropna()        # diff in one field(one day)
-
-    #this preservers dimensions off diff values
-    up_chg = 0 * diff
-    down_chg = 0 * diff
-    
-    # up change is equal to the positive difference, otherwise equal to zero
-    up_chg[diff > 0] = diff[ diff>0 ]
-    
-    # down change is equal to negative deifference, otherwise equal to zero
-    down_chg[diff < 0] = diff[ diff < 0 ]
-    
-    # check pandas documentation for ewm
-    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.ewm.html
-    # values are related to exponential decay
-    # we set com=time_window-1 so we get decay alpha=1/time_window
-    up_chg_avg   = up_chg.ewm(com=time_window-1 , min_periods=time_window).mean()
-    down_chg_avg = down_chg.ewm(com=time_window-1 , min_periods=time_window).mean()
-    
-    rs = abs(up_chg_avg/down_chg_avg)
-    rsi = 100 - 100/(1+rs)
-    return rsi
-
-"""
-Ham nay giup cho lam muot do thi 
-tinh chi so EMA
-input la 1 cot gia tri cua ma co phieu
-output cot gia tri cua ma co phieu day nhung muot hon
-"""
-def calEMA(data, com = 0.5):
-    ema = data.ewm(com= com).mean()
-    return ema
 
 def getPrices(code):
 
